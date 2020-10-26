ecc55825dbd932e1e6b1e0454db7d9525839a91c
Ileana Valdez <ileanavaldez@Ileanas-MBP.wireless.yale.internal>
2020-10-26 13:25:43 -0400

    react hooks

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Grid } from 'semantic-ui-react';
 import EventForm from '../eventForm/EventForm';
 import EventList from './EventList';
+import {sampleData} from '../../../app/api/sampleData'
 
 export default function EventDashboard() {
+    const [events, setEvents] = useState(sampleData);
+
     return (
         <Grid>
             <Grid.Column width={10}>
-                <EventList />
+                <EventList events={events} />
             </Grid.Column>
             <Grid.Column width={6}>
                 <EventForm />
