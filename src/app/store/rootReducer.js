28a06657e97f0a03fedbfcca2d10ebf0c89674ed
Ileana Valdez <ileanavaldez@Ileanas-MacBook-Pro.local>
2020-12-07 12:17:38 -0700

    user profiles

--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -4,13 +4,15 @@ import testReducer from '../../features/sandbox/testReducer'
 import authReducer from '../../features/auth/authReducer'
 import modalReducer from '../common/modals/modalReducer';
 import asyncReducer from '../async/asyncReducer';
+import profileReducer from '../../features/profiles/profilePage/profileReducer';
 
 const rootReducer = combineReducers({
     test: testReducer,
     event: eventReducer,
     modals: modalReducer,
     auth: authReducer,
-    async: asyncReducer
+    async: asyncReducer,
+    profile: profileReducer
 })
 
 export default rootReducer;
--- src/features/nav/SignedInMenu.jsx
+++ src/features/nav/SignedInMenu.jsx
@@ -6,7 +6,7 @@ import { signOutFirebase } from "../../app/firestore/firebaseService";
 import {toast} from "react-toastify";
 
 export default function SignedInMenu() {
-  const { currentUser } = useSelector((state) => state.auth);
+  const { currentUserProfile } = useSelector((state) => state.profile);
   const history = useHistory();
 
   async function handleSignOut() {
@@ -22,9 +22,9 @@ export default function SignedInMenu() {
       <Image
         avatar
         spaced="right"
-        src={currentUser.photoURL || "/assets/user.png"}
+        src={currentUserProfile.photoURL || "/assets/user.png"}
       />
-      <Dropdown pointing="top left" text={currentUser.displayName}>
+      <Dropdown pointing="top left" text={currentUserProfile.displayName}>
         <Dropdown.Menu>
           <Dropdown.Item
             as={Link}
@@ -32,7 +32,7 @@ export default function SignedInMenu() {
             text="Create Event"
             icon="plus"
           />
-          <Dropdown.Item text="My Profile" icon="user" />
+          <Dropdown.Item as={Link} to={`/profile/${currentUserProfile.id}`} text="My Profile" icon="user" />
           <Dropdown.Item as={Link} to='/account' text="My Account" icon="settings" />
           <Dropdown.Item
             onClick={handleSignOut}
